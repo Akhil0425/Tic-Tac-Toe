b488bc37f876d5afc2981057979c1466b39dcf1d
Akhil0425 <kambamakhil25@gmail.com>
2023-02-24 19:56:00 +0530

    tic-tac

--- tic-tac/src/App.js
+++ tic-tac/src/App.js
@@ -1,36 +1,28 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-function Square(){
-  const [value, setValue] = useState(null);
-
-  function handleClick(){
-    setValue('X');
-  }
-  return(
-    <button className="square" onClick={handleClick} >
-      {value}
-    </button>
-  )
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
-export default function board(){
-  return(
-    <div>
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-      <Square value="4" />
-      <Square value="5" />
-      <Square value="6" />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-      <Square value="7" />
-      <Square value="8" />
-      <Square value="9" />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
-    </div>
-  )
+    </>
+  );
 }
