3db8831d85215e5d456696061630306b822fc6af
Akhil0425 <kambamakhil25@gmail.com>
2023-02-24 19:50:19 +0530

    tic-tac

--- tic-tac/src/App.js
+++ tic-tac/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square({value}){
+  return <button className="square">{value}</button>;
 }
 
-export default App;
+export default function board(){
+  return(
+    <div>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+      <Square value="4" />
+      <Square value="5" />
+      <Square value="6" />
+      </div>
+      <div className="board-row">
+      <Square value="7" />
+      <Square value="8" />
+      <Square value="9" />
+      </div>
+    </div>
+  )
+}
